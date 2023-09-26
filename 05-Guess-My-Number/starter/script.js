baa3fac9f185ede8bf4ce89aee5e2c64ff86da56
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-09-26 17:11:16 -0500

    More lessons completed
    DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+/*
+console.log(document.querySelector('.message'));
+
+document.querySelector(`.message`).textContent = 'Correct Number!';
+
+document.querySelector(`.number`).textContent = 13;
+document.querySelector(`.score`).textContent = 11;
+
+document.querySelector(`.guess`).value = 23;
+console.log(document.querySelector(`.guess`).value);
+*/
