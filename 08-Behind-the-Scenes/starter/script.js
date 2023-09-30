46635d4c93e3ce61ca777f0d728727e14695493b
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-09-30 11:18:54 -0500

    Another lesson
    Scope and the scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
@@ -6,6 +6,19 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `You are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var str = 'You are a millenial';
+    } else {
+      var str = `You are not a millenial`;
+    }
+
+    console.log(str);
+
+    function add(a, b) {
+      return a + b;
+    }
+    add(1, 2);
   }
   printAge();
 
