8d10ef19454b7ba9ef4404fbb4c3fbd30fc073d4
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-09-30 11:04:14 -0500

    Further lessons
    Javascript overview, javascript engine, execution contexts

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Zack';
+calcAge(2001);
