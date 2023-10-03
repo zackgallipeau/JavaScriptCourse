e6a0a97e3ffcce0f3865d8ad7c4a67729d4bb95c
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-10-03 17:27:39 -0500

    Another lesson
    For-of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,85 @@
 'use strict';
 ////////////////////////
+
 /*
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//step 1
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+//step 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+
+//step 3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//step 4
+const players1Final = [...players1, `Thiago`, `Coutinho`, `Perisic`];
+
+//step 5
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+//step 6
+function printGoals(...players) {
+  const didScore = { ...game.scored };
+  console.log(
+    `A total of ${players.length} goals were scored by ${[...game.scored]}`
+  );
+}
+
+printGoals(...game.scored);
+
+//step 7
+team1 < team2 && console.log(`Team 1 is more likely to win`);
+team1 > team2 && console.log(`Team 2 is more likely to win`);
+*/
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -55,6 +134,21 @@ const restaurant = {
   },
 };
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+//old way
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`);
+}
+
+//new way
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+/*
 const rest1 = {
   name: `Capri`,
   numGuests: 20,
