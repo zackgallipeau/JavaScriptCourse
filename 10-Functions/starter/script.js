310308496ba6937f262d0d8938edc018aa2442d3
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-10-13 13:24:45 -0500

    Another lesson
    First class function/higher-order function explanation

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -10,8 +10,35 @@ const createBooking = function (flightNum, numPassengers, price) {
     numPassengers,
     price,
   };
+  //
   console.log(booking);
   bookings.push(booking);
 };
 
 createBooking('LH123');
+
+const flightNumber = 'LH234';
+const jonas = {
+  fullName: 'Jonas Schmedtmann',
+  passport: 2391283490,
+};
+
+const checkIn = function (flightNum, passenger) {
+  passenger.name = 'Mr. ' + passenger.fullName;
+
+  if (passenger.passport === 2391283490) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport, buddy');
+  }
+};
+// checkIn(flightNumber, jonas);
+// console.log(flightNumber);
+// console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flightNumber, jonas);
