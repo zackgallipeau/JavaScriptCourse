61abd7e9c61b566cf6424bc1933ee0c4ea353a46
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-10-13 12:31:39 -0500

    Another lesson
    Default parameters in functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers, price) {
+  numPassengers = numPassengers || 1;
+  price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
