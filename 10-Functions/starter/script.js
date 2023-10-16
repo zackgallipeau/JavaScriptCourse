10518f64df75de9801192b2fc45870e9ada65d39
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-10-16 12:09:02 -0500

    Coding challenge
    Closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -199,7 +199,7 @@ document
 })();
 
 (() => console.log('this will only run once'))();
-*/
+
 
 //closures
 const secureBooking = function () {
@@ -220,3 +220,49 @@ secureBooking()();
 booker();
 secureBooking()();
 booker();
+
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+h();
+f();
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`we are now boarding all ${n} passengers`);
+
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 2134023942309;
+boardPassengers(180, 3);
+console.log(perGroup);
+*/
+
+(function () {
+  const header = document.querySelector(`h1`);
+  header.style.color = 'red';
+  document.querySelector(`body`).addEventListener(`click`, function () {
+    header.style.color = 'blue';
+  });
+})();
