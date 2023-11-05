eb2f352d99ecc2ea21c8871851216d07c1b2760a
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-11-05 10:22:06 -0600

    More lessons
    Leaflet API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -12,13 +12,41 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
+  //checks if location access is available
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      //renders the map on page load
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        L.marker(mapEvent.latlng)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
-      alert('Could not get your position');
+      alert('Could not get your position'); //if locationi access is not available
     }
   );
 }
