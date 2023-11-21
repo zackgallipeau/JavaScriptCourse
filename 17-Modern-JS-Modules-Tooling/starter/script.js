3000352251dcc706c737062ac0cd48f163ed5a37
zackgallipeau <145948320+zackgallipeau@users.noreply.github.com>
2023-11-20 21:59:46 -0600

    more lessons
    imports/exports, await outside of functions

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -10,3 +10,25 @@ console.log(cart[0]);
 import add from './shoppingCart.js';
 add('soap');
 console.log(cart);
+
+const getLastPost = async function () {
+  const result = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  const data = await result.json();
+  //   console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+//Not very clean :(
+//getLastPost().then(last => console.log(last));
+
+//better method
+console.log(await getLastPost());
+
+(function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+});
